a2da2f8b13bae24c89f410d4d2ca664ba4eab3e0
azizam68 <abdoul-aziz.mbodj@oclock.school>
2026-09-23 10:38:21 +0200

    server.js responsability is to start app.js and to listen

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
 import "dotenv/config"
+
 import express from "express"
 import path from "path"
+
 import healthRoutes from "./routes/health.routes.js"
-import { notFound } from "./middlewares/notFound.js"
 import usersRouter from "./routes/users.routes.js"
+
+import { notFound } from "./middlewares/notFound.js"
 import { errorMiddleware } from "./middleware/error.middleware.js"
 
 const __dirname = path.resolve(".")
 
-const PORT = process.env.PORT || process.env.APP_PORT || 3000
-
 const app = express()
+
 app.use(express.json())
+
 app.use(express.static(path.join(__dirname, "./public")))
 
 app.use("/api", healthRoutes)
 app.use("/api/users", usersRouter)
 
+// 404 : doit être après toutes les routes
 app.use(notFound)
+
+// Erreurs : doit être le dernier middleware
 app.use(errorMiddleware)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+export default app
